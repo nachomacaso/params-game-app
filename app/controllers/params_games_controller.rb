9ebc16d79ea94bd3c62f63230b013e0230ce86f6
nachomacaso <nathanmacaso@gmail.com>
2016-08-30 20:48:25 -0500

    Better defined actions in Controller and added URL segment params

--- app/controllers/params_games_controller.rb
+++ app/controllers/params_games_controller.rb
@@ -1,12 +1,36 @@
 class ParamsGamesController < ApplicationController
   def name_game
-    @response = params[:response]
-    @message = "Hey your name starts with the first letter of the alphabet!!"
+    @response = params[:response].upcase
   end
+
+  def a_name_game
+    @response = params[:response]
+    if @response != nil
+      if @response.start_with?("A") || @response.start_with?("") 
+        @message = "Hey your name starts with the first letter of the alphabet!!"
+      end
+    end
+  end
+
   def number_game
     @response = params[:response]
     @answer = 57
     @right_message = "You guessed correctly! #{@response} was the correct answer!"
     @wrong_message = "Sorry try again"
+  end
+
+  def url_guess
+    response = params[:turkey].to_i
+    answer = 69
+
+    if response == answer
+      @message = "You got it correct!"
+    elsif response < 1 || response > 100
+      @message = "You are out of bounds."
+    elsif response > answer
+      @message = "Guess lower."
+    elsif response < answer
+      @message = "Guess higher."
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/name' => 'params_games#name_game'
+  get '/a_name' => 'params_games#a_name_game'
   get '/number' => 'params_games#number_game'
+  get '/guess_number/:turkey' => 'params_games#url_guess'
 end
